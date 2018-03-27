a518eff788b81f48b60c690ed682fb9f5256b0c1
Shianne Lee <shiannelee@live.ca>
2018-03-27 01:41:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,14 +11,29 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var price = Math.floor(Math.random() * 100)
+ var newObj = {[item]: price}
+ cart.push(newObj)
+ console.log(`${item} has been added to your cart.`)
+ return cart
 }
 
 function viewCart() {
   // write your code here
+  if(cart.length === 0){
+    return console.log(`Your shopping cart is empty.`)
+  }else{
+
+  }
 }
 
 function total() {
   // write your code here
+  var total = 0
+  for(var i in cart){
+    total = total + cart[i]
+  }
+  return total
 }
 
 function removeFromCart(item) {
