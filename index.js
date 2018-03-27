171b7805535812da0249e4bf70a50041dfdf1cb9
Shianne Lee <shiannelee@live.ca>
2018-03-27 03:56:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function removeFromCart(item) {
   for(var i in cart){
     var cartItem = Object.keys(cart[i]);
     if(cartItem === item){
-      delete cart[i][item];
+      delete cart[i];
       return cart;
     }else{
       console.log(`That item is not in your cart.`);
